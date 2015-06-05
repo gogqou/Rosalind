bf15962944e6d07889c1dcca0d739f415905bfd1
Grace Ou <grace.ou@berkeley.edu>
2015-06-04 18:31:53 -0700

    with comments

--- Rosalind_solutions/recurring_rabbits.py
+++ Rosalind_solutions/recurring_rabbits.py
@@ -7,6 +7,11 @@ author: gogqou
 import sys
 def recur(time, k, mapping):
     if time not in mapping.keys():
+        #if I haven't calculated it (since I'm going top down)
+        #I'll go back one step and use my definitions of the number of 
+        # reproducing rabbits (a) and new rabbits (b) 
+        # to calculate my current number of rabbits, and save that to the 
+        # mapping
         c = recur(time-1, k, mapping)
         a = c[0]
         b = c[1]
@@ -23,8 +28,10 @@ def main():
     litter_size = int(sys.argv[2])
     rabbits = {}
     rabbits[1] = [0,1]
+    #get a mapping that sets the initial conditions
     new_rabbits = recur(num_months, litter_size, rabbits)
     sum = new_rabbits[0]+new_rabbits[1]
+    #total rabbits = new + reproducing rabbits
     print sum
     return sum
 if __name__ == '__main__':
