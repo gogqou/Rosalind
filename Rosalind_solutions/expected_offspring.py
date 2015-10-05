6d1da4ac18c9822bb72163e49025bbf4f9f70859
Grace Ou <grace.ou@berkeley.edu>
2015-10-05 16:00:05 -0700

    expected offspring solution

--- Rosalind_solutions/expected_offspring.py
+++ Rosalind_solutions/expected_offspring.py
@@ -1,5 +1,15 @@
 '''
 Created on Oct 5, 2015
+Given: Six positive integers, each of which does not exceed 20,000. The integers correspond to the number of couples in a population possessing each genotype pairing for a given factor. In order, the six given integers represent the number of couples having the following genotypes:
+
+    AA-AA
+    AA-Aa
+    AA-aa
+    Aa-Aa
+    Aa-aa
+    aa-aa
+
+Return: The expected number of offspring displaying the dominant phenotype in the next generation, under the assumption that every couple has exactly two offspring.
 
 @author: gogqou
 '''
